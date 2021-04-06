5db8f7f1559ebddd660c77fdfb0a31241f2c5802
David-Ade-Creator <www.davidadeyemi@gmail.com>
2021-04-06 10:47:00 +0530

    add middleware and create/delete post

---
+++ graphql/typeDefs.js
@@ -0,0 +1,34 @@
+const { gql } = require("apollo-server");
+
+module.exports = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+  type User {
+    id: ID!
+    email: String!
+    firstName: String!
+    lastName: String!
+    token: String!
+    username: String!
+    createdAt: String!
+  }
+  input RegisterInput {
+    firstName: String!
+    lastName: String!
+    username: String!
+    password: String!
+    confirmPassword: String!
+    email: String!
+  }
+  type Query {
+    getPosts: [Post]
+  }
+  type Mutation {
+    register(registerInput: RegisterInput): User!
+    login(username: String!, password: String!): User!
+  }
+`;
