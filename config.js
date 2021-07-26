957d1cd860ac8ca22b9df0f64bfc2e0853d25f13
David-Ade-Creator <www.davidadeyemi@gmail.com>
2021-07-26 18:14:38 +0530

    made changes for hosting reasons

---
+++ config.js
@@ -0,0 +1,10 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+module.exports = {
+    MONGODB : "mongodb+srv://standave:9pGujWP66lRHVblY@cluster0.grsx4.mongodb.net/goslin?retryWrites=true&w=majority",
+    SECRET_KEY : "somethingsecret",
+    ACCESSBUCKETKEY : "AKIAJ26QJGKEFTY3WMHA",
+    SECRETACCESSBUCKETKEY : "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"
+}
